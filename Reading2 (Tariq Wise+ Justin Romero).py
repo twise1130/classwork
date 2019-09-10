98ca1c1f6f6c6e2e48d4790c3ff97378efbb2b35
twise1130 <53017274+twise1130@users.noreply.github.com>
2019-09-10 14:16:45 -0400

    Add files via upload

---
+++ Reading2 (Tariq Wise+ Justin Romero).py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+rating = input('enter an interger rating between 1 and 10')
+
+
+# In[2]:
+
+
+type(rating)
+
+
+# In[38]:
+
+
+if grade >=90:
+    print('congratulations your grade of 90 earns you an A in this Course')
+
+
+# In[39]:
+
+
+grade = 90
+
+
+# In[57]:
+
+
+r=2
+diameter=2*r
+pi=3.14159
+circumfrence=2*pi*r
+area=pi*r**2
+
+
+# In[51]:
+
+
+
+
